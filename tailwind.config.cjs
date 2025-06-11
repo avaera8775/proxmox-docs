77383a89b35ae419e9a895cc374674903a269523
Johan Sörman <johan.sorman@cgi.com>
2025-06-11 09:21:58 +0200

    Fix darkmode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 // tailwind.config.js
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: 'media', // Use OS preference for dark mode
   theme: {
     extend: {},
   },
